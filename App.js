3704251d4b7d8108b7dbcbfb330a40e755f1cd8e
Alexander Sherwin <alx.sherwin@gmail.com>
2021-07-28 16:06:42 -0600

    save spot for the day

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, SafeAreaView, Alert } from 'react-native';
+import { useDimensions } from '@react-native-community/hooks';
 
 export default function App() {
+
+  const image = {
+    uri: 'https://reactnative.dev/img/tiny_logo.png',
+    width: 128,
+    height: 128,
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{backgroundColor: "orange"}}>
+      <Button
+        color="orange"
+        title="Click Me, Senpai" 
+        onPress={
+          () => Alert.prompt("My Title", "My Message", text => console.log(text))
+        }
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f1f1f1',
     alignItems: 'center',
     justifyContent: 'center',
   },
